093cd39f5af715fc8ad91869e7bbae16a7e7e264
greatm <manoj.great@yahoo.com>
2013-04-17 15:08:08 +0530

    add_all_04/17_5_06

---
+++ b/Migrations/201304170937303_po.cs
@@ -0,0 +1,32 @@
+namespace b.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class po : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.PurchaseOrders",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Date = c.DateTime(nullable: false),
+                        Remarks = c.String(),
+                        Vendor_ID = c.Int(),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.Vendors", t => t.Vendor_ID)
+                .Index(t => t.Vendor_ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.PurchaseOrders", new[] { "Vendor_ID" });
+            DropForeignKey("dbo.PurchaseOrders", "Vendor_ID", "dbo.Vendors");
+            DropTable("dbo.PurchaseOrders");
+        }
+    }
+}
--- b/Models/bDBContext.cs
+++ b/Models/bDBContext.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using b.ViewModels;
 
 namespace b.Models
 {
     public class bDBContext : DbContext
     {
         public DbSet<UserProfile> UserProfiles { get; set; }
+        
         public DbSet<Vendor> Vendors { get; set; }
-
         public DbSet<Product> Products { get; set; }
+
+        public DbSet<PurchaseOrder> PurchaseOrders { get; set; }
     }
 }
