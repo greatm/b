36523d61309bb538e48d23c17f9c9bb2c3f49882
greatm <manoj.great@yahoo.com>
2013-04-23 09:13:07 +0530

    commit_date_04/23_time_9_12

---
+++ b/Controllers/ReportUsersController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Syncfusion.ReportViewer.Mvc;
+using Syncfusion.Reports.Mvc;
+
+namespace b.Controllers
+{
+    public class ReportUsersController : Controller
+    {
+        ReportViewerModel model { get; set; }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult View(ReportViewerParams parameter)
+        {
+            //this.Session["InvoiceID"] = parameter.InvoiceID;
+            return View(model);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult ViewReport(ReportViewerParams param)
+        {
+            return new ReportViewerHtmlActionResult(this.GetModel(), param);
+        }
+        [HttpPost]
+        public ActionResult ReportAction(ReportViewerParams parameter)
+        {
+            model = new ReportViewerModel();
+            model.ReportPath = Server.MapPath("~/RDL/employee.rdl");
+            if (Session["InvoiceID"] != null)
+            {
+                List<ReportParameter> parameters = new List<ReportParameter>();
+                ReportParameter param = new ReportParameter();
+                param.Labels.Add(Session["InvoiceID"].ToString());
+                param.Values.Add(Session["InvoiceID"].ToString());
+                param.Name = "InvoiceID";
+                parameters.Add(param);
+                model.Parameters = parameters;
+            }
+            return new ReportViewerHtmlActionResult(model, parameter);
+        }
+        ReportViewerModel GetModel()
+        {
+            model = new ReportViewerModel();
+            model.ReportPath = Server.MapPath("~/RDL/employee.rdl");
+            return model;
+        }
+    }
+}
