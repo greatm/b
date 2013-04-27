6f949230b0b19e94bc82862e7d952ce45ee90f7e
greatm <manoj.great@yahoo.com>
2013-04-27 14:08:30 +0530

    commit_date_04/27_time_3_30

--- b/ViewModels/SalesOrder.cs
+++ b/ViewModels/SalesOrder.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace b.ViewModels
 {
     public class SalesOrder
     {
         public int ID { get; set; }
+
+        [Required]
         public DateTime Date { get; set; }
         public int CustomerID { get; set; }
+
+        [StringLength (5,ErrorMessage="always keep remarks short and simple")]
         public string Remarks { get; set; }
         public IEnumerable<SalesOrderItem> SalesOrderItems { get; set; }
         public decimal TotalAmount { get; set; }
