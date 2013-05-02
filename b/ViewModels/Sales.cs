33f548a6f8800088e7fc302e7b8797098d3bcff9
greatm <manoj.great@yahoo.com>
2013-05-02 18:20:22 +0530

    commit_date_05/02_time_8_16

--- b/ViewModels/Sales.cs
+++ b/ViewModels/Sales.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using b.Models;
 
 namespace b.ViewModels
 {
@@ -14,5 +15,6 @@ namespace b.ViewModels
         public string Invoice { get; set; }
         public string Remarks { get; set; }
         public IList<SalesItem> SalesItems { get; set; }
+        public PackingList PackingList { get; set; }
     }
 }
