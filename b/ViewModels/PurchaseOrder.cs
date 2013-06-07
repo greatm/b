9b7b74d1e26f9f6f3c0ea65214e2b80e3faa3ffb
greatm <manoj.great@yahoo.com>
2013-06-07 16:14:50 +0530

    commit_date_06/07_time_6_09

--- b/ViewModels/PurchaseOrder.cs
+++ b/ViewModels/PurchaseOrder.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace b.ViewModels
 {
@@ -10,8 +11,9 @@ namespace b.ViewModels
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime Date { get; set; }
-        public virtual Vendor Vendor { get; set; }
         public virtual int VendorID { get; set; }
+        [ForeignKey("VendorID")]
+        public virtual Vendor Vendor { get; set; }
         public virtual int StoreID { get; set; }
         public virtual IList<POItem> POItems { get; set; }
     }
