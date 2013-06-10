d47e6534fe9a46ebb6750f71ad691920cc2943d2
greatm <manoj.great@yahoo.com>
2013-06-10 17:21:24 +0530

    commit_date_06/10_time_7_20

---
+++ b/pdf/PdfViewController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace b.pdf
+{
+    public class PdfViewController : Controller
+    {
+        private readonly HtmlViewRenderer htmlViewRenderer;
+        private readonly StandardPdfRenderer standardPdfRenderer;
+        public PdfViewController()
+        {
+            this.htmlViewRenderer = new HtmlViewRenderer();
+            this.standardPdfRenderer = new StandardPdfRenderer();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ b/pdf/Renderer.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Text;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using iTextSharp.text;
+using iTextSharp.text.html.simpleparser;
+using iTextSharp.text.pdf;
+
+namespace b.pdf
+{
+    public class HtmlViewRenderer
+    {
+        public string RenderViewToString(Controller controller, string viewName, object viewData)
+        {
+            var renderedView = new StringBuilder();
+            using (var responseWriter = new StringWriter(renderedView))
+            {
+                var fakeResponse = new HttpResponse(responseWriter);
+                var fakeContext = new HttpContext(HttpContext.Current.Request, fakeResponse);
+                var fakeControllerContext = new ControllerContext(new HttpContextWrapper(fakeContext), controller.ControllerContext.RouteData, controller.ControllerContext.Controller);
+
+                var oldContext = HttpContext.Current;
+                HttpContext.Current = fakeContext;
+
+                using (var viewPage = new ViewPage())
+                {
+                    var html = new HtmlHelper(CreateViewContext(responseWriter, fakeControllerContext), viewPage);
+                    html.RenderPartial(viewName, viewData);
+                    HttpContext.Current = oldContext;
+                }
+            }
+
+            return renderedView.ToString();
+        }
+
+        private static ViewContext CreateViewContext(TextWriter responseWriter, ControllerContext fakeControllerContext)
+        {
+            return null;
+            //return new ViewContext(fakeControllerContext, new FakeView(), new ViewDataDictionary(), new TempDataDictionary(), responseWriter);
+        }
+    }
+    public class StandardPdfRenderer
+    {
+        private const int HorizontalMargin = 40;
+        private const int VerticalMargin = 40;
+
+        public byte[] Render(string htmlText, string pageTitle)
+        {
+            byte[] renderedBuffer;
+
+            using (var outputMemoryStream = new MemoryStream())
+            {
+                using (var pdfDocument = new Document(PageSize.A4, HorizontalMargin, HorizontalMargin, VerticalMargin, VerticalMargin))
+                {
+                    PdfWriter pdfWriter = PdfWriter.GetInstance(pdfDocument, outputMemoryStream);
+                    pdfWriter.CloseStream = false;
+                    pdfWriter.PageEvent = new PrintHeaderFooter { Title = pageTitle };
+                    pdfDocument.Open();
+                    using (var htmlViewReader = new StringReader(htmlText))
+                    {
+                        using (var htmlWorker = new HTMLWorker(pdfDocument))
+                        {
+                            htmlWorker.Parse(htmlViewReader);
+                        }
+                    }
+                }
+
+                renderedBuffer = new byte[outputMemoryStream.Position];
+                outputMemoryStream.Position = 0;
+                outputMemoryStream.Read(renderedBuffer, 0, renderedBuffer.Length);
+            }
+
+            return renderedBuffer;
+        }
+    }
+    public class PrintHeaderFooter : PdfPageEventHelper
+    {
+        private PdfContentByte pdfContent;
+        private PdfTemplate pageNumberTemplate;
+        private BaseFont baseFont;
+        private DateTime printTime;
+
+        public string Title { get; set; }
+
+        public override void OnOpenDocument(PdfWriter writer, Document document)
+        {
+            printTime = DateTime.Now;
+            baseFont = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
+            pdfContent = writer.DirectContent;
+            pageNumberTemplate = pdfContent.CreateTemplate(50, 50);
+        }
+
+        public override void OnStartPage(PdfWriter writer, Document document)
+        {
+            base.OnStartPage(writer, document);
+
+            Rectangle pageSize = document.PageSize;
+
+            if (Title != string.Empty)
+            {
+                pdfContent.BeginText();
+                pdfContent.SetFontAndSize(baseFont, 11);
+                pdfContent.SetRGBColorFill(0, 0, 0);
+                pdfContent.SetTextMatrix(pageSize.GetLeft(40), pageSize.GetTop(40));
+                pdfContent.ShowText(Title);
+                pdfContent.EndText();
+            }
+        }
+
+        public override void OnEndPage(PdfWriter writer, Document document)
+        {
+            base.OnEndPage(writer, document);
+
+            int pageN = writer.PageNumber;
+            string text = pageN + " - ";
+            float len = baseFont.GetWidthPoint(text, 8);
+
+            Rectangle pageSize = document.PageSize;
+            pdfContent = writer.DirectContent;
+            pdfContent.SetRGBColorFill(100, 100, 100);
+
+            pdfContent.BeginText();
+            pdfContent.SetFontAndSize(baseFont, 8);
+            pdfContent.SetTextMatrix(pageSize.Width / 2, pageSize.GetBottom(30));
+            pdfContent.ShowText(text);
+            pdfContent.EndText();
+
+            pdfContent.AddTemplate(pageNumberTemplate, (pageSize.Width / 2) + len, pageSize.GetBottom(30));
+
+            pdfContent.BeginText();
+            pdfContent.SetFontAndSize(baseFont, 8);
+            pdfContent.ShowTextAligned(PdfContentByte.ALIGN_RIGHT, printTime.ToString(), pageSize.GetRight(40), pageSize.GetBottom(30), 0);
+            pdfContent.EndText();
+        }
+
+        public override void OnCloseDocument(PdfWriter writer, Document document)
+        {
+            base.OnCloseDocument(writer, document);
+
+            pageNumberTemplate.BeginText();
+            pageNumberTemplate.SetFontAndSize(baseFont, 8);
+            pageNumberTemplate.SetTextMatrix(0, 0);
+            pageNumberTemplate.ShowText(string.Empty + (writer.PageNumber - 1));
+            pageNumberTemplate.EndText();
+        }
+    }
+}
